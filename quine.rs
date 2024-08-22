b2855ce61e4daf82b1e5eb762145e3a2921f7f32
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-08-22 17:15:29 +0530

    Some simple cleanups

--- quine.rs
+++ quine.rs
@@ -7,29 +7,33 @@ const ROW_COUNT: usize = 5;
 const COL_COUNT: usize = 3;
 
 fn main() {
-    let q = "fn main() {\n    let q = \"?\";\n\n    for c in q.chars().collect::<Vec<char>>() {\n        if c as u8 == 63 {\n            for c in q.chars() {\n                match c {\n                    '\\n' => print!(\"\\\\n\"),\n                    '\"' => print!(\"\\\\\\\"\"),\n                    '\\\\' => print!(\"\\\\\\\\\"),\n                    c => print!(\"{c}\"),\n                }\n                continue;\n            }\n        } else {\n            print!(\"{c}\");\n        }\n    }\n}\n";
+    interpret_quine();
+}
+
+fn interpret_quine() {
+    let q = "const NUMBER_COUNT: usize = 11;\nconst NUMBER: [i32; NUMBER_COUNT] = [\n31599, 19812, 14479, 31207, 23524, 29411, 29679, 30866, 31727, 31719, 1040,\n];\n\nconst ROW_COUNT: usize = 5;\nconst COL_COUNT: usize = 3;\n\nfn main() {\ninterpret_quine();\n}\n\nfn interpret_quine() {\nlet q = \"?\";\n\nfor c in q.chars().collect::<Vec<char>>() {\nif c as u8 == 63 {\nfor c in q.chars() {\nmatch c {\n'\\n' => print!(\"\\\\n\"),\n'\"' => print!(\"\\\\\\\"\"),\n'\\\\' => print!(\"\\\\\\\\\"),\nc => print!(\"{c}\"),\n}\ncontinue;\n}\n} else {\nprint!(\"{c}\");\n}\n}\n\nprintln!(\"\");\n}\n\nfn _print_number(number: usize) {\nfor y in 0..ROW_COUNT {\nfor x in 0..COL_COUNT {\nlet c = (ROW_COUNT - y - 1) * COL_COUNT + x;\nif ((NUMBER[number] >> c) & 1) == 1 {\nprint!(\"*\");\n} else {\nprint!(\" \");\n}\n}\nprintln!(\"\");\n}\nprintln!(\"\");\n}\n";
 
     for c in q.chars().collect::<Vec<char>>() {
         if c as u8 == 63 {
             for c in q.chars() {
-                match c {
-                    '\n' => print!("\\n"),
-                    '"' => print!("\\\""),
-                    '\\' => print!("\\\\"),
-                    c => print!("{c}"),
+                if c == '\n' {
+                    print!("\\n");
+                } else if c == '"' {
+                    print!("\\\"");
+                } else if c == '\\' {
+                    print!("\\\\");
+                } else {
+                    print!("{c}");
                 }
-                continue;
             }
         } else {
             print!("{c}");
         }
     }
     println!("");
-
-    print_number(7);
 }
 
-fn print_number(number: usize) {
+fn _print_number(number: usize) {
     for y in 0..ROW_COUNT {
         for x in 0..COL_COUNT {
             let c = (ROW_COUNT - y - 1) * COL_COUNT + x;
